77ed34db2a985d2f3e19b30119877b295e6eb73c
Sameep30k <you@example.com>
2024-07-29 18:05:28 +0530

    update

--- src/main.tsx
+++ src/main.tsx
@@ -40,7 +40,7 @@ import { AuthProvider } from "./context/AuthContext";
 import { Toaster } from "react-hot-toast";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:5000/api/v1";
+axios.defaults.baseURL = "https://mernappbackend-lmpd.onrender.com/api/v1";
 axios.defaults.withCredentials = true;
 
 const theme = createTheme({
