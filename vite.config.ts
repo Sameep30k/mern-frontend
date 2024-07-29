becaa9a0861e0000bec3c59245b2e74b0916476a
Sameep30k <you@example.com>
2024-07-29 17:21:37 +0530

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 3000, // Adjust this value as needed
+  },
+})
+
+
+
